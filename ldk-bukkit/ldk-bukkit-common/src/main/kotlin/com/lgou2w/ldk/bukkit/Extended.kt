db4c013b353401af386e09f0ad758e805acf2daa
MoonLake <mcmoonlake@hotmail.com>
2018-09-22 14:32:00 +0800

    Bukkit task scheduler extension function support

--- ldk-bukkit/ldk-bukkit-common/src/main/kotlin/com/lgou2w/ldk/bukkit/Extended.kt
+++ ldk-bukkit/ldk-bukkit-common/src/main/kotlin/com/lgou2w/ldk/bukkit/Extended.kt
@@ -16,4 +16,76 @@
 
 package com.lgou2w.ldk.bukkit
 
+import com.lgou2w.ldk.common.Callable
+import com.lgou2w.ldk.common.Runnable
+import org.bukkit.Bukkit
+import org.bukkit.plugin.Plugin
+import org.bukkit.scheduler.BukkitTask
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.FutureTask
 
+/**************************************************************************
+ *
+ * org.bukkit.plugin.Plugin RunTask Extended
+ *
+ **************************************************************************/
+
+fun Plugin.runTask(task: Runnable) : BukkitTask
+        = Bukkit.getScheduler().runTask(this, task)
+
+fun Plugin.runTaskLater(task: Runnable, delay: Long) : BukkitTask
+        = Bukkit.getScheduler().runTaskLater(this, task, delay)
+
+fun Plugin.runTaskTimer(task: Runnable, delay: Long, period: Long) : BukkitTask
+        = Bukkit.getScheduler().runTaskTimer(this, task, delay, period)
+
+fun Plugin.runTaskAsync(task: Runnable) : BukkitTask
+        = Bukkit.getScheduler().runTaskAsynchronously(this, task)
+
+fun Plugin.runTaskAsyncLater(task: Runnable, delay: Long) : BukkitTask
+        = Bukkit.getScheduler().runTaskLaterAsynchronously(this, task, delay)
+
+fun Plugin.runTaskAsyncTimer(task: Runnable, delay: Long, period: Long) : BukkitTask
+        = Bukkit.getScheduler().runTaskTimerAsynchronously(this, task, delay, period)
+
+fun BukkitTask?.cancelTask()
+        = this?.cancel()
+
+/**************************************************************************
+ *
+ * org.bukkit.plugin.Plugin CallTask Future Extended
+ *
+ **************************************************************************/
+
+private fun <T> Plugin.callTaskFuture(callback: Callable<T>, delay: Long = 0L, async: Boolean = false) : CompletableFuture<T> {
+    val future = CompletableFuture<T>()
+    val futureTask = FutureTask(callback)
+    val task : Runnable = {
+        try {
+            futureTask.run()
+            future.complete(futureTask.get())
+        } catch (e: Exception) {
+            future.completeExceptionally(e)
+        }
+    }
+    if (delay <= 0) {
+        if (async) runTaskAsync(task)
+        else task.invoke()  // blocking server threads, Until the task is completed
+    } else {
+        if (async) runTaskAsyncLater(task, delay)
+        else runTaskLater(task, delay) // blocking server threads after waiting for a delay, Until the task is completed
+    }
+    return future
+}
+
+fun <T> Plugin.callTaskFuture(callback: Callable<T>) : CompletableFuture<T>
+        = callTaskFuture(callback, 0L)
+
+fun <T> Plugin.callTaskFutureLater(callback: Callable<T>, delay: Long) : CompletableFuture<T>
+        = callTaskFuture(callback, delay)
+
+fun <T> Plugin.callTaskFutureAsync(callback: Callable<T>) : CompletableFuture<T>
+        = callTaskFuture(callback, 0L, true)
+
+fun <T> Plugin.callTaskFutureAsyncLater(callback: Callable<T>, delay: Long) : CompletableFuture<T>
+        = callTaskFuture(callback, delay, true)
