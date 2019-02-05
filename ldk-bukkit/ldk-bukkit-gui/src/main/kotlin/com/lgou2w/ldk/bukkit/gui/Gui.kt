ccce8893ce7fa8f0c5aa9bc09eabf5b5c69968d3
lgou2w <lgou2w@hotmail.com>
2019-02-05 15:24:52 +0800

    License bracket symbol problem adjustment

--- ldk-bukkit/ldk-bukkit-gui/src/main/kotlin/com/lgou2w/ldk/bukkit/gui/Gui.kt
+++ ldk-bukkit/ldk-bukkit-gui/src/main/kotlin/com/lgou2w/ldk/bukkit/gui/Gui.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The lgou2w (lgou2w@hotmail.com)
+ * Copyright (C) 2018 The lgou2w <lgou2w@hotmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
