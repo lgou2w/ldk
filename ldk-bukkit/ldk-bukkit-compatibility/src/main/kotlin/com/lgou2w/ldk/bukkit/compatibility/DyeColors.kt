49530290314b410a5fd2ccd54e9f241a94096322
lgou2w <lgou2w@hotmail.com>
2019-03-03 15:15:01 +0800

    DyeColors compatibility test and adjustments

--- ldk-bukkit/ldk-bukkit-compatibility/src/main/kotlin/com/lgou2w/ldk/bukkit/compatibility/DyeColors.kt
+++ ldk-bukkit/ldk-bukkit-compatibility/src/main/kotlin/com/lgou2w/ldk/bukkit/compatibility/DyeColors.kt
@@ -1,40 +1,74 @@
+/*
+ * Copyright (C) 2018 25 (https://github.com/25)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Copyright (C) 2018 The lgou2w <lgou2w@hotmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.lgou2w.ldk.bukkit.compatibility
 
 import com.lgou2w.ldk.common.notNull
 import org.bukkit.DyeColor
 
-enum class DyeColors(private val pre113dyecolor: String, private val post113dyecolor: String) {
+/**
+ * @since LDK 0.1.8-rc
+ */
+enum class DyeColors(private val alias: String? = null) {
 
-    BLACK("BLACK", "BLACK"),
-    BLUE("BLUE", "BLUE"),
-    BROWN("BROWN", "BROWN"),
-    CYAN("CYAN", "CYAN"),
-    GRAY("GRAY", "GRAY"),
-    GREEN("GREEN", "GREEN"),
-    LIGHT_BLUE("LIGHT_BLUE", "LIGHT_BLUE"),
-    LIGHT_GRAY("LIGHT_GRAY", "SILVER"),
-    LIME("LIME", "LIME"),
-    MAGENTA("MAGENTA", "MAGENTA"),
-    ORANGE("ORANGE", "ORANGE"),
-    PINK("PINK", "PINK"),
-    PURPLE("PURPLE", "PURPLE"),
-    RED("RED", "RED"),
-    WHITE("WHITE", "WHITE"),
-    YELLOW("YELLOW", "YELLOW");
+    BLACK,
+    BLUE,
+    BROWN,
+    CYAN,
+    GRAY,
+    GREEN,
+    LIGHT_BLUE,
+    LIGHT_GRAY("SILVER"), // Before 1.12: Silver, After 1.13: Light gray
+    LIME,
+    MAGENTA,
+    ORANGE,
+    PINK,
+    PURPLE,
+    RED,
+    WHITE,
+    YELLOW,
+    ;
 
-    private var cached: DyeColor? = null
+    private var valid : DyeColor? = null
 
     fun toBukkit(): DyeColor {
-        return if (cached == null) {
-            try {
-                cached = DyeColor.valueOf(pre113dyecolor)
-                cached as DyeColor
-            } catch (ignore: IllegalArgumentException) {
-                cached = DyeColor.valueOf(post113dyecolor)
-                cached as DyeColor
+        return if (valid == null) {
+            valid = try {
+                DyeColor.valueOf(alias ?: name)
+            } catch (e: IllegalArgumentException) {
+                DyeColor.valueOf(name)
             }
-        } else {
-            cached.notNull()
-        }
+            valid.notNull()
+        } else
+            valid.notNull()
     }
 }
