b756567ba4b94c6876b0d5bf2891dcaa1b701824
lgou2w <lgou2w@hotmail.com>
2019-05-13 13:51:11 +0800

    ldk-chat unit test passed

--- ldk-chat/src/test/kotlin/com/lgou2w/ldk/chat/ChatSerializerTest.kt
+++ ldk-chat/src/test/kotlin/com/lgou2w/ldk/chat/ChatSerializerTest.kt
@@ -16,10 +16,13 @@
 
 package com.lgou2w.ldk.chat
 
+import com.google.gson.JsonParseException
 import com.google.gson.JsonSyntaxException
 import org.amshove.kluent.invoking
 import org.amshove.kluent.shouldBeInstanceOf
+import org.amshove.kluent.shouldContain
 import org.amshove.kluent.shouldEqual
+import org.amshove.kluent.shouldNotContain
 import org.amshove.kluent.shouldNotEqual
 import org.amshove.kluent.shouldThrow
 import org.junit.Test
@@ -52,5 +55,111 @@ class ChatSerializerTest {
         ChatSerializer.fromRaw(null) shouldEqual ecct
         ChatSerializer.fromRaw("") shouldEqual ecct
         ChatSerializer.toJson(ecct) shouldEqual "{\"text\":\"\"}"
+    }
+
+    @Test fun `ChatSerializer - fromRaw - Invalid color should be white`() {
+        val raw = "§pInvalid color code"
+        val component = ChatSerializer.fromRaw(raw)
+        component.toRaw() shouldEqual raw
+    }
+
+    @Test fun `ChatSerializer - fromRaw` () {
+        val raw = "&a&l&n&o&m&kHello&r&gWorld!"
+        val component = ChatSerializer.fromRaw(raw)
+        component.toRaw(false) shouldEqual raw.toColor().stripColor()
+    }
+
+    @Test fun `ChatSerializer - RawMessage`() {
+        val name = "com.lgou2w.ldk.chat.ChatSerializer\$RawMessage"
+        val clazz = try {
+            ChatSerializerTest::class.java.classLoader.loadClass(name)
+        } catch (e: ClassNotFoundException) {
+            return
+        }
+        val inst = clazz.getConstructor(String::class.java).newInstance("") // empty
+        val get = clazz.getDeclaredMethod("get")
+        get.isAccessible = true
+        get.invoke(inst) shouldBeInstanceOf ChatComponentText::class
+    }
+
+    @Test fun `ChatSerializer - serialize - validation`() {
+        ChatComponentText().append("extraText")
+            .toJson() shouldContain "extra" // extra JsonArray
+        ChatComponentTranslation("key")
+            .toJson() shouldNotContain "with" // empty withs
+        ChatComponentTranslation("key").addWiths(arrayOf(ChatComponentText("arg")))
+            .toJson() shouldContain "with" // with JsonArray
+    }
+
+    class MyComponent : ChatComponentAbstract()
+
+    @Test fun `ChatSerializer - serialize - Special type should not be serialize`() {
+        invoking { MyComponent().toJson() } shouldThrow JsonParseException::class
+    }
+
+    @Test fun `ChatSerializer - deserialize - validation`() {
+        (ChatSerializer.fromJson("[\"CCT1\",\"CCT2\"]").extras.last() as ChatComponentText)
+            .text shouldEqual "CCT2" // JsonArray
+        (ChatSerializer.fromJson("{\"translate\":\"key\"}") as ChatComponentTranslation)
+            .withs.isEmpty() shouldEqual true // empty withs
+        (ChatSerializer.fromJson("{\"translate\":\"key\",\"with\":[\"arg\"]}") as ChatComponentTranslation)
+            .withs.first() shouldEqual "arg" // with ChatComponentText.text
+        ((ChatSerializer.fromJson("{\"translate\":\"key\",\"with\":[{\"text\":\"arg\",\"color\":\"red\"}]}") as ChatComponentTranslation)
+            .withs.first() as ChatComponentText).style.getColor() shouldEqual ChatColor.RED
+        ((ChatSerializer.fromJson("{\"translate\":\"key\",\"with\":[{\"keybind\":\"ctrl\"}]}") as ChatComponentTranslation)
+            .withs.first() as ChatComponentKeybind).keybind shouldEqual "ctrl"
+        (ChatSerializer.fromJson("{\"text\":\"\",\"extra\":[\"Hello\",\"World\"]}") as ChatComponentText)
+            .toRaw() shouldEqual "HelloWorld"
+    }
+
+    @Test fun `ChatSerializer - deserialize - The name and objective of the score should be`() {
+        invoking { ChatSerializer.fromJson("{\"score\":{}}") } shouldThrow JsonParseException::class
+        invoking { ChatSerializer.fromJson("{\"score\":{\"name\":\"test\"}}") } shouldThrow JsonParseException::class
+        (ChatSerializer.fromJson("{\"score\":{\"name\":\"test\",\"objective\":\"target\",\"value\":\"v\"}}") as ChatComponentScore)
+            .name shouldEqual "test"
+    }
+
+    @Test fun `ChatSerializer - deserialize - Don't know how to parse should throw exception`() {
+        invoking { ChatSerializer.fromJson("{\"invalid\":\"chat\"}") } shouldThrow JsonParseException::class
+    }
+
+    @Test fun `ChatSerializer - deserialize - Empty extra should throw exception`() {
+        invoking { ChatSerializer.fromJson("{\"text\":\"\",\"extra\":[]}") } shouldThrow JsonParseException::class
+        (ChatSerializer.fromJson("{\"text\":\"\",\"extra\":[\"CCT\"]}") as ChatComponentText)
+            .toRaw() shouldEqual "CCT"
+    }
+
+    @Test fun `ChatSerializer - serialize - style - validation`() {
+        ChatComponentText().setStyle(ChatStyle().setBold(true).setColor(null))
+            .toJson() shouldNotContain "color"
+        ChatComponentText().setStyle(ChatStyle()
+            .setBold(true)
+            .setItalic(true)
+            .setUnderlined(true)
+            .setStrikethrough(true)
+            .setObfuscated(true)
+            .setInsertion("Insertion")
+            .setClickEvent(ChatClickEvent(ChatClickEvent.Action.OPEN_URL, "github.com"))
+            .setHoverEvent(ChatHoverEvent(ChatHoverEvent.Action.SHOW_TEXT, ChatComponentText("hi")))
+        ).toJson() shouldContain "Insertion"
+        ChatComponentText().setStyle(ChatStyle()
+            .setHoverEvent(ChatHoverEvent(ChatHoverEvent.Action.SHOW_ITEM, ChatSerializer.ChatComponentRaw("raw")))
+        ).toJson() shouldContain "raw"
+    }
+
+    @Test fun `ChatSerializer - deserialize - style - validation`() {
+        ChatSerializer.fromJson("{\"text\":\"\",\"bold\":true}")
+            .style.bold shouldEqual true
+        ChatSerializer.fromJson("{\"text\":\"\",\"bold\":true,\"italic\":true,\"underlined\":true,\"strikethrough\":true,\"obfuscated\":true," +
+                                "\"insertion\":\"Insertion\",\"clickEvent\":{\"action\":\"open_url\",\"value\":\"github.com\"}," +
+                                "\"hoverEvent\":{\"action\":\"show_text\",\"value\":[\"hi\"]}}"
+        ).toRaw(false) shouldEqual ""
+    }
+
+    @Test fun `ChatSerializer - deserialize - style - Click and hover should be valid`() {
+        ChatSerializer.fromJson("{\"text\":\"\",\"hoverEvent\":\"invalid\",\"clickEvent\":\"invalid\"}}")
+            .style.hoverEvent shouldEqual null
+        ChatSerializer.fromJson("{\"text\":\"\",\"hoverEvent\":{\"action\":\"invalid\"},\"clickEvent\":{\"action\":\"invalid\"}}")
+            .style.hoverEvent shouldEqual null
     }
 }
--- ldk-chat/src/test/kotlin/com/lgou2w/ldk/chat/ChatStyleTest.kt
+++ ldk-chat/src/test/kotlin/com/lgou2w/ldk/chat/ChatStyleTest.kt
@@ -16,8 +16,10 @@
 
 package com.lgou2w.ldk.chat
 
+import org.amshove.kluent.invoking
 import org.amshove.kluent.shouldEqual
 import org.amshove.kluent.shouldNotEqual
+import org.amshove.kluent.shouldThrow
 import org.junit.Test
 
 class ChatStyleTest {
@@ -53,9 +55,58 @@ class ChatStyleTest {
         cs2.getStrikethrough() shouldEqual false
         cs2.getUnderlined() shouldEqual false
         cs2.getObfuscated() shouldEqual false
+        cs2.getClickEvent() shouldEqual null
+        cs2.getHoverEvent() shouldEqual null
+        cs2.getInsertion() shouldEqual null
         cs.equals(cs2) shouldEqual false
+        cs2.equals(cs) shouldEqual false
         cs2.setParent(cs)
         cs2.getColor() shouldEqual ChatColor.RED
         cs2.equals(cs) shouldEqual false
+    }
+
+    @Test fun `ChatStyle - equals`() {
+        val cs1 = ChatStyle()
+        val cs2 = ChatStyle()
+        cs1.italic = true
+        cs1.equals(cs2) shouldEqual false
+        cs2.italic = cs1.italic
+        cs1.underlined = true
+        cs1.equals(cs2) shouldEqual false
+        cs2.underlined = cs1.underlined
+        cs1.strikethrough = true
+        cs1.equals(cs2) shouldEqual false
+        cs2.strikethrough = cs1.strikethrough
+        cs1.obfuscated = true
+        cs1.equals(cs2) shouldEqual false
+        cs2.obfuscated = cs1.obfuscated
+        cs1.clickEvent = ChatClickEvent(ChatClickEvent.Action.OPEN_URL, "github.com")
+        cs1.equals(cs2) shouldEqual false
+        cs2.clickEvent = cs1.clickEvent
+        cs1.hoverEvent = ChatHoverEvent(ChatHoverEvent.Action.SHOW_ITEM, ChatComponentText("hi"))
+        cs1.equals(cs2) shouldEqual false
+        cs2.hoverEvent = cs1.hoverEvent
+        cs1.insertion = "Insertion"
+        cs1.equals(cs2) shouldEqual false
+        cs2.insertion = cs1.insertion
+        cs1.equals(cs2) shouldEqual true
+    }
+
+    @Test fun `ChatStyle - ROOT - These operations should not be supported`() {
+        val rootField = ChatStyle::class.java.getDeclaredField("ROOT")
+                       ?: return
+        rootField.isAccessible = true
+        val root = rootField.get(null) as ChatStyle
+        invoking { root.setParent(null) } shouldThrow UnsupportedOperationException::class
+        invoking { root.setColor(null) } shouldThrow UnsupportedOperationException::class
+        invoking { root.setBold(null) } shouldThrow UnsupportedOperationException::class
+        invoking { root.setItalic(null) } shouldThrow UnsupportedOperationException::class
+        invoking { root.setStrikethrough(null) } shouldThrow UnsupportedOperationException::class
+        invoking { root.setUnderlined(null) } shouldThrow UnsupportedOperationException::class
+        invoking { root.setObfuscated(null) } shouldThrow UnsupportedOperationException::class
+        invoking { root.setClickEvent(null) } shouldThrow UnsupportedOperationException::class
+        invoking { root.setHoverEvent(null) } shouldThrow UnsupportedOperationException::class
+        invoking { root.setInsertion(null) } shouldThrow UnsupportedOperationException::class
+        root.toString() shouldEqual "ChatStyle.ROOT"
     }
 }
