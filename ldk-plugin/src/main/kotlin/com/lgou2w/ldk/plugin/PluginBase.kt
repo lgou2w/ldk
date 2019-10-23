0e02c34aac67dd3a907ba3860697153a0f9f5237
lgou2w <lgou2w@hotmail.com>
2019-10-23 22:29:11 +0800

    Add ldk-plugin module implements

---
+++ ldk-plugin/src/main/kotlin/com/lgou2w/ldk/plugin/PluginBase.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2016-2019 The lgou2w <lgou2w@hotmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lgou2w.ldk.plugin
+
+abstract class PluginBase : Plugin {
+
+  final override val name : String
+    get() = metadata.name
+
+  override fun equals(other: Any?): Boolean {
+    if (other === this)
+      return true
+    if (other is Plugin)
+      return name == other.name
+    return false
+  }
+
+  override fun hashCode(): Int {
+    return name.hashCode()
+  }
+}
