38295b0f4c1fdfb3413cff61afa151a84c52dc4e
lgou2w <lgou2w@hotmail.com>
2021-02-20 00:43:21 +0800

    feat: add ldk-bukkit-packet module and some features

--- ldk-bukkit-chat/src/main/java/com/lgou2w/ldk/bukkit/chat/ChatFactory.java
+++ ldk-bukkit-chat/src/main/java/com/lgou2w/ldk/bukkit/chat/ChatFactory.java
@@ -16,9 +16,197 @@
 
 package com.lgou2w.ldk.bukkit.chat;
 
+import com.google.gson.Gson;
+import com.lgou2w.ldk.bukkit.packet.PacketFactory;
+import com.lgou2w.ldk.bukkit.version.BukkitVersion;
+import com.lgou2w.ldk.chat.ChatComponent;
+import com.lgou2w.ldk.chat.ChatSerializer;
+import com.lgou2w.ldk.common.Enums;
+import com.lgou2w.ldk.reflect.ConstructorAccessor;
+import com.lgou2w.ldk.reflect.ConstructorReflectionMatcher;
+import com.lgou2w.ldk.reflect.FieldAccessor;
+import com.lgou2w.ldk.reflect.FuzzyReflection;
+import com.lgou2w.ldk.reflect.MethodAccessor;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.TestOnly;
+
+import java.lang.reflect.Modifier;
+import java.util.UUID;
+import java.util.function.Supplier;
+
+import static com.lgou2w.ldk.bukkit.reflect.MinecraftReflection.getMinecraftClass;
+import static com.lgou2w.ldk.bukkit.reflect.MinecraftReflection.getMinecraftClassOrNull;
+
 public final class ChatFactory {
 
   private ChatFactory() { }
 
-  // TODO: ChatFactory
+  /// NMS Classes
+
+  @NotNull public final static Class<?> CLASS_ICHAT_BASE_COMPONENT;
+  @NotNull public final static Class<?> CLASS_CHAT_SERIALIZER;
+  @NotNull public final static Class<?> CLASS_PACKET_PLAY_OUT_CHAT;
+  @Nullable public final static Class<?> CLASS_CHAT_MESSAGE_TYPE;
+
+  // TODO: docs
+  @NotNull final static Class<?> CLASS_GSON_RELOCATED;
+  final static boolean GSON_RELOCATED;
+
+  static {
+    try {
+      CLASS_ICHAT_BASE_COMPONENT = getMinecraftClass("IChatBaseComponent");
+      CLASS_CHAT_SERIALIZER = getMinecraftClass("IChatBaseComponent$ChatSerializer", "ChatSerializer");
+      CLASS_PACKET_PLAY_OUT_CHAT = getMinecraftClass("PacketPlayOutChat");
+      CLASS_CHAT_MESSAGE_TYPE = getMinecraftClassOrNull("ChatMessageType");
+      Class<?> gsonRelocated = null;
+      try {
+        gsonRelocated = Class.forName("org.bukkit.craftbukkit.libs.com.google.gson.Gson");
+      } catch (ClassNotFoundException ignore) {
+      } finally {
+        CLASS_GSON_RELOCATED = gsonRelocated != null ? gsonRelocated : Gson.class;
+        GSON_RELOCATED = gsonRelocated != Gson.class;
+      }
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException("Error in initializing internal static block: ", e);
+    }
+  }
+
+  /// NMS Accessors
+
+  // 1.16 before    NMS.PacketPlayOutChat -> public constructor(NMS.IChatBaseComponent, NMS.ChatMessageType | byte)
+  // 1.16 and after NMS.PacketPlayOutChat -> public constructor(NMS.IChatBaseComponent, NMS.ChatMessageType, UUID)
+  @SuppressWarnings("ConstantConditions")
+  final static Supplier<@NotNull ConstructorAccessor<Object>> CONSTRUCTOR_PACKET_PLAY_OUT_CHAT
+    = FuzzyReflection.lazySupplier(CLASS_PACKET_PLAY_OUT_CHAT, true, fuzzy -> {
+      ConstructorReflectionMatcher<Object> matcher = fuzzy.useConstructorMatcher();
+      if (!BukkitVersion.isV116OrLater) {
+        return matcher
+          .withArgs(CLASS_ICHAT_BASE_COMPONENT, CLASS_CHAT_MESSAGE_TYPE != null ? CLASS_CHAT_MESSAGE_TYPE : byte.class)
+          .resultAccessor("Missing match: NMS.PacketPlayOutChat -> Constructor(NMS.IChatBaseComponent, " +
+            CLASS_CHAT_MESSAGE_TYPE != null ? "NMS.ChatMessageType)" : "byte)");
+      } else {
+        return matcher
+          .withArgs(CLASS_ICHAT_BASE_COMPONENT, CLASS_CHAT_MESSAGE_TYPE, UUID.class)
+          .resultAccessor("Missing match: NMS.PacketPlayOutChat -> Constructor(NMS.IChatBaseComponent, NMS.ChatMessageType, UUID)");
+      }
+    });
+
+  // NMS.ChatSerializer -> public static Gson GSON;
+  final static Supplier<@NotNull FieldAccessor<Object, Gson>> FIELD_CHAT_SERIALIZER_GSON
+    = FuzzyReflection.lazySupplier(CLASS_CHAT_SERIALIZER, true, fuzzy -> fuzzy
+    .useFieldMatcher()
+    .withModifiers(Modifier.STATIC)
+    .withType(CLASS_GSON_RELOCATED)
+    .resultAccessorAs("Missing match: NMS.ChatSerializer -> public final static Gson GSON;"));
+
+  /** @see ChatFactory#CLASS_GSON_RELOCATED */
+  @Nullable final static Supplier<@NotNull MethodAccessor<Object, String>> METHOD_GSON_RELOCATED_TO_JSON
+    = !GSON_RELOCATED ? null : FuzzyReflection.lazySupplier(CLASS_GSON_RELOCATED, fuzzy -> fuzzy
+    .useMethodMatcher()
+    .withoutModifiers(Modifier.STATIC)
+    .withModifiers(Modifier.PUBLIC)
+    .withArgs(String.class, Class.class)
+    .withName("toJson")
+    .resultAccessorAs());
+
+  /** @see ChatFactory#CLASS_GSON_RELOCATED */
+  @Nullable final static Supplier<@NotNull MethodAccessor<Object, String>> METHOD_GSON_RELOCATED_FROM_JSON
+    = !GSON_RELOCATED ? null : FuzzyReflection.lazySupplier(CLASS_GSON_RELOCATED, fuzzy -> fuzzy
+    .useMethodMatcher()
+    .withoutModifiers(Modifier.STATIC)
+    .withModifiers(Modifier.PUBLIC)
+    .withArgs(String.class, Class.class)
+    .withName("fromJson")
+    .resultAccessorAs());
+
+  /// Public API
+
+  // TODO: from, to
+
+  @NotNull
+  @Contract("null -> fail; !null -> !null")
+  @SuppressWarnings({ "ConstantConditions", "CastCanBeRemovedNarrowingVariableType" })
+  public static ChatComponent from(Object chat) {
+    if (chat == null) throw new NullPointerException("chat");
+    if (!CLASS_ICHAT_BASE_COMPONENT.isInstance(chat))
+      throw new IllegalArgumentException("Value type of the instance does not match. (Expected: " + CLASS_ICHAT_BASE_COMPONENT + ")");
+    Object gson = FIELD_CHAT_SERIALIZER_GSON.get().get(null);
+    String json;
+    if (!GSON_RELOCATED) {
+      json = ((Gson) gson).toJson(chat, CLASS_ICHAT_BASE_COMPONENT);
+    } else {
+      json = METHOD_GSON_RELOCATED_TO_JSON.get().invoke(gson, chat, CLASS_ICHAT_BASE_COMPONENT);
+    }
+    return ChatSerializer.fromJson(json);
+  }
+
+  @NotNull
+  @Contract("null -> fail; !null -> !null")
+  @SuppressWarnings({ "ConstantConditions", "CastCanBeRemovedNarrowingVariableType" })
+  public static Object to(ChatComponent chat) {
+    if (chat == null) throw new NullPointerException("chat");
+    Object gson = FIELD_CHAT_SERIALIZER_GSON.get().get(null);
+    String json = ChatSerializer.toJson(chat);
+    if (!GSON_RELOCATED) {
+      return ((Gson) gson).fromJson(json, CLASS_ICHAT_BASE_COMPONENT);
+    } else {
+      return METHOD_GSON_RELOCATED_FROM_JSON.get().invoke(gson, json, CLASS_ICHAT_BASE_COMPONENT);
+    }
+  }
+
+  // TODO: 是否公开此方法
+  @NotNull
+  @Contract("null, _, _ -> fail")
+  public static Object newPacketChat(
+    ChatComponent chat,
+    @Nullable ChatType type,
+    @Nullable UUID sender
+  ) {
+    if (chat == null) throw new NullPointerException("chat");
+    if (type == null) type = ChatType.CHAT;
+
+    Object origin = to(chat);
+    Object messageType = CLASS_CHAT_MESSAGE_TYPE != null
+      ? Enums.fromOrdinal(CLASS_CHAT_MESSAGE_TYPE, type.ordinal())
+      : type.ordinal();
+    if (!BukkitVersion.isV116OrLater) {
+      return CONSTRUCTOR_PACKET_PLAY_OUT_CHAT.get().newInstance(origin, messageType);
+    } else {
+      if (sender == null) sender = new UUID(0L, 0L); // zero is always display message
+      return CONSTRUCTOR_PACKET_PLAY_OUT_CHAT.get().newInstance(origin, messageType, sender);
+    }
+  }
+
+  @Contract("null, _, _, _ -> fail; _, null, _, _ -> fail")
+  public static void sendChat(
+    Player receiver,
+    ChatComponent chat,
+    @Nullable ChatType type,
+    @Nullable UUID sender
+  ) {
+    if (receiver == null) throw new NullPointerException("receiver");
+    Object packet = newPacketChat(chat, type, sender);
+    PacketFactory.sendPacket(packet, receiver);
+  }
+
+  @Contract("null, _, _ -> fail; _, null, _ -> fail")
+  public static void sendChat(Player receiver, ChatComponent chat, @Nullable ChatType type) {
+    sendChat(receiver, chat, type, null);
+  }
+
+  @Contract("null, _ -> fail; _, null -> fail")
+  public static void sendChat(Player receiver, ChatComponent chat) {
+    sendChat(receiver, chat, null, null);
+  }
+
+  // FIXME: remove unit test
+  @TestOnly
+  @Deprecated
+  public static void test() {
+    System.out.println(CONSTRUCTOR_PACKET_PLAY_OUT_CHAT.get());
+    System.out.println(FIELD_CHAT_SERIALIZER_GSON.get());
+  }
 }
---
+++ ldk-bukkit-packet/src/main/java/com/lgou2w/ldk/bukkit/packet/PacketFactory.java
@@ -0,0 +1,216 @@
+/*
+ * Copyright (C) 2016-2021 The lgou2w <lgou2w@hotmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lgou2w.ldk.bukkit.packet;
+
+import com.lgou2w.ldk.reflect.FieldAccessor;
+import com.lgou2w.ldk.reflect.FuzzyReflection;
+import com.lgou2w.ldk.reflect.MethodAccessor;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.TestOnly;
+
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+import static com.lgou2w.ldk.bukkit.reflect.MinecraftReflection.getCraftBukkitClass;
+import static com.lgou2w.ldk.bukkit.reflect.MinecraftReflection.getMinecraftClass;
+
+public final class PacketFactory {
+
+  private PacketFactory() { }
+
+  /// NMS Classes
+
+  @NotNull public final static Class<?> CLASS_PACKET;
+  @NotNull public final static Class<?> CLASS_PACKET_LISTENER;
+  @NotNull public final static Class<?> CLASS_NETWORK_MANAGER;
+  @NotNull public final static Class<?> CLASS_PLAYER_CONNECTION;
+  @NotNull public final static Class<?> CLASS_ENTITY;
+  @NotNull public final static Class<?> CLASS_ENTITY_PLAYER;
+  @NotNull public final static Class<?> CLASS_CRAFT_ENTITY;
+
+  static {
+    try {
+      CLASS_PACKET = getMinecraftClass("Packet");
+      CLASS_PACKET_LISTENER = getMinecraftClass("PacketListener");
+      CLASS_NETWORK_MANAGER = getMinecraftClass("NetworkManager");
+      CLASS_PLAYER_CONNECTION = getMinecraftClass("PlayerConnection");
+      CLASS_ENTITY = getMinecraftClass("Entity");
+      CLASS_ENTITY_PLAYER = getMinecraftClass("EntityPlayer");
+      CLASS_CRAFT_ENTITY = getCraftBukkitClass("entity.CraftEntity");
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException("Error in initializing internal static block: ", e);
+    }
+  }
+
+  /// NMS Accessors
+
+  // OBC.entity.CraftEntity -> public NMS.Entity getHandle();
+  final static Supplier<@NotNull MethodAccessor<Object, Object>> METHOD_CRAFT_ENTITY_GET_HANDLE
+    = FuzzyReflection.lazySupplier(CLASS_CRAFT_ENTITY, true, fuzzy -> fuzzy
+    .useMethodMatcher()
+    .withoutModifiers(Modifier.STATIC)
+    .withModifiers(Modifier.PUBLIC)
+    .withType(CLASS_ENTITY)
+    .resultAccessor("Missing match: OBC.entity.CraftEntity -> Method: public NMS.Entity getHandle()"));
+
+  // NMS.EntityPlayer -> public NMS.PlayerConnection playerConnection;
+  final static Supplier<@NotNull FieldAccessor<Object, Object>> FIELD_ENTITY_PLAYER_CONNECTION
+    = FuzzyReflection.lazySupplier(CLASS_ENTITY_PLAYER, true, fuzzy -> fuzzy
+    .useFieldMatcher()
+    .withoutModifiers(Modifier.STATIC)
+    .withType(CLASS_PLAYER_CONNECTION)
+    .resultAccessor());
+
+  // NMS.PlayerConnection -> public NMS.NetworkManager networkManager;
+  final static Supplier<@NotNull FieldAccessor<Object, Object>> FIELD_PLAYER_CONNECTION_MANAGER
+    = FuzzyReflection.lazySupplier(CLASS_PLAYER_CONNECTION, true, fuzzy -> fuzzy
+    .useFieldMatcher()
+    .withoutModifiers(Modifier.STATIC)
+    .withType(CLASS_NETWORK_MANAGER)
+    .resultAccessor());
+
+  // NMS.PlayerConnection -> public void sendPacket(NMS.Packet);
+  final static Supplier<@NotNull MethodAccessor<Object, Object>> METHOD_PLAYER_CONNECTION_SEND_PACKET
+    = FuzzyReflection.lazySupplier(CLASS_PLAYER_CONNECTION, true, fuzzy -> fuzzy
+    .useMethodMatcher()
+    .withoutModifiers(Modifier.STATIC)
+    .withArgs(CLASS_PACKET)
+    .resultAccessor());
+
+  // NMS.Packet -> public void process(NMS.PacketListener);
+  final static Supplier<@NotNull MethodAccessor<Object, Object>> METHOD_PACKET_PROCESS
+    = FuzzyReflection.lazySupplier(CLASS_PACKET, true, fuzzy -> fuzzy
+    .useMethodMatcher()
+    .withoutModifiers(Modifier.STATIC)
+    .withArgs(CLASS_PACKET_LISTENER)
+    .resultAccessor());
+
+  @NotNull
+  @SuppressWarnings("ConstantConditions")
+  static Object getPlayerHandle(@NotNull Player player) {
+    return METHOD_CRAFT_ENTITY_GET_HANDLE.get().invoke(player);
+  }
+
+  static void validatePacket(Object packet) {
+    if (!CLASS_PACKET.isInstance(packet))
+      throw new IllegalArgumentException("Value type of the instance does not match. (Expected: " + CLASS_PACKET + ")");
+  }
+
+  static void sendPacket0(Object packet, Player receiver) {
+    Object handle = getPlayerHandle(receiver);
+    Object connection = FIELD_ENTITY_PLAYER_CONNECTION.get().get(handle);
+    METHOD_PLAYER_CONNECTION_SEND_PACKET.get().invoke(connection, packet);
+  }
+
+  static void processPacket0(Object packet, Player sender) {
+    Object handle = getPlayerHandle(sender);
+    Object connection = FIELD_ENTITY_PLAYER_CONNECTION.get().get(handle);
+    METHOD_PACKET_PROCESS.get().invoke(packet, connection);
+  }
+
+  @Contract("null, _ -> fail")
+  static Player[] nearbyPlayers(Location center, double range) {
+    if (center == null) throw new NullPointerException("center");
+    World world = center.getWorld();
+    if (world == null)
+      throw new IllegalArgumentException("The world at this location is a null.");
+
+    List<Player> result = new ArrayList<>();
+    double squared = range < 1.0 ? 1.0 : range * range;
+    for (Player player : world.getPlayers()) {
+      if (player.getLocation().distanceSquared(center) <= squared)
+        result.add(player);
+    }
+    return result.toArray(new Player[0]);
+  }
+
+  /// Public API
+
+  @Contract("null, _ -> fail; _, null -> fail")
+  public static void sendPacket(Object packet, Player receiver) {
+    validatePacket(packet);
+    if (receiver == null) throw new NullPointerException("receiver");
+    sendPacket0(packet, receiver);
+  }
+
+  @Contract("null, _ -> fail; _, null -> fail")
+  public static void sendPacket(Object packet, Player[] receivers) {
+    validatePacket(packet);
+    if (receivers == null) throw new NullPointerException("receivers");
+    for (Player receiver : receivers)
+      sendPacket0(packet, receiver);
+  }
+
+  @Contract("null -> fail")
+  public static void sendPacketToAll(Object packet) {
+    validatePacket(packet);
+    sendPacket(packet, Bukkit.getOnlinePlayers().toArray(new Player[0]));
+  }
+
+  @Contract("null, _, _ -> fail; _, null, _ -> fail")
+  public static void sendPacketToNearby(Object packet, Location center, double range) {
+    validatePacket(packet);
+    Player[] players = nearbyPlayers(center, range);
+    sendPacket(packet, players);
+  }
+
+  @Contract("null, _ -> fail; _, null -> fail")
+  public static void processPacket(Object packet, Player sender) {
+    validatePacket(packet);
+    if (sender == null) throw new NullPointerException("sender");
+    processPacket0(packet, sender);
+  }
+
+  @Contract("null, _ -> fail; _, null -> fail")
+  public static void processPacket(Object packet, Player[] senders) {
+    validatePacket(packet);
+    if (senders == null) throw new NullPointerException("senders");
+    for (Player sender : senders)
+      processPacket0(packet, sender);
+  }
+
+  @Contract("null -> fail")
+  public static void processPacketToAll(Object packet) {
+    validatePacket(packet);
+    processPacket(packet, Bukkit.getOnlinePlayers().toArray(new Player[0]));
+  }
+
+  @Contract("null, _, _ -> fail; _, null, _ -> fail")
+  public static void processPacketToNearby(Object packet, Location center, double range) {
+    validatePacket(packet);
+    Player[] players = nearbyPlayers(center, range);
+    processPacket(packet, players);
+  }
+
+  // FIXME: remove unit test
+  @TestOnly
+  @Deprecated
+  public static void test() {
+    System.out.println(METHOD_CRAFT_ENTITY_GET_HANDLE.get());
+    System.out.println(FIELD_ENTITY_PLAYER_CONNECTION.get());
+    System.out.println(FIELD_PLAYER_CONNECTION_MANAGER.get());
+    System.out.println(METHOD_PLAYER_CONNECTION_SEND_PACKET.get());
+    System.out.println(METHOD_PACKET_PROCESS.get());
+  }
+}
