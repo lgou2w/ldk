dd7c26552e834dad11f985ba78dfdf94c206ce3e
lgou2w <lgou2w@hotmail.com>
2021-02-23 17:46:26 +0800

    feat: redesign the Enchantment class

--- ldk-bukkit-basic/src/main/java/com/lgou2w/ldk/bukkit/item/Enchantment.java
+++ ldk-bukkit-basic/src/main/java/com/lgou2w/ldk/bukkit/item/Enchantment.java
@@ -16,142 +16,103 @@
 
 package com.lgou2w.ldk.bukkit.item;
 
-import com.lgou2w.ldk.bukkit.version.BukkitVersion;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
 import java.util.Objects;
 
-public enum Enchantment {
+public final class Enchantment {
+  @NotNull private final String namespacedKey;
+  @NotNull private final String namespace;
+  @NotNull private final String key;
+  // legacy Minecraft 1.12 and before
+  @Nullable private final Integer id;
 
-  PROTECTION(0, "protection_environmental", "protection"),
-  FIRE_PROTECTION(1, "protection_fire", "fire_protection"),
-  FEATHER_FALLING(2, "protection_fall", "feather_falling"),
-  BLAST_PROTECTION(3, "protection_explosions", "blast_protection"),
-  PROJECTILE_PROTECTION(4, "protection_projectile", "projectile_protection"),
-  RESPIRATION(5, "oxygen", "respiration"),
-  AQUA_AFFINITY(6, "water_worker", "aqua_affinity"),
-  THORNS(7, "thorns"),
-  DEPTH_STRIDER(8, "depth_strider"),
-  FROST_WALKER(9, "frost_walker"),
-  BINDING_CURSE(10, "binding_curse"),
-  SHARPNESS(16, "damage_all", "sharpness"),
-  SMITE(17, "damage_undead", "smite"),
-  BANE_OF_ARTHROPODS(18, "damage_arthropods", "bane_of_arthropods"),
-  KNOCKBACK(19, "knockback"),
-  FIRE_ASPECT(20, "fire_aspect"),
-  LOOTING(21, "loot_bonus_mobs", "looting"),
-  SWEEPING(22, "sweeping_edge", "sweeping"),
-  EFFICIENCY(32, "dig_speed", "efficiency"),
-  SILK_TOUCH(33, "silk_touch"),
-  UNBREAKING(34, "durability", "unbreaking"),
-  FORTUNE(35, "loot_bonus_blocks", "fortune"),
-  POWER(48, "arrow_damage", "power"),
-  PUNCH(49, "arrow_knockback", "punch"),
-  FLAME(50, "arrow_fire", "flame"),
-  INFINITE(51, "arrow_infinite", "infinite"),
-  LUCK_OF_THE_SEA(61, "luck", "luck_of_the_sea"),
-  LURE(62, "lure"),
-  LOYALTY(65, "loyalty"),
-  IMPALING(66, "impaling"),
-  RIPTIDE(67, "riptide"),
-  CHANNELING(68, "channeling"),
-  MENDING(70, "mending"),
-  VANISHING_CURSE(71, "vanishing_curse"),
-
-  ///
-  /// From here on, these enchantment id are all maintained by LDK, since Minecraft 1.13 has completely removed the Id.
-  /// 从这里开始, 这些附魔的 Id 都由 LDK 自行维护, 因为自从 Minecraft 1.13 已经完全移除 Id 了.
-  ///
-
-  MULTISHOT(200, "multishot"),
-  QUICK_CHARGE(201, "quick_charge"),
-  PIERCING(202, "piercing"),
-  SOUL_SPEED(203, "soul_speed"),
-  ;
-
-  private final int id;
-  private final String legacy, flatting;
-
-  Enchantment(int id, String legacy, String flatting) {
+  @Contract("null, _, _ -> fail; _, null, _ -> fail")
+  Enchantment(String namespace, String key, @Nullable Integer id) {
+    this.namespace = Objects.requireNonNull(namespace, "namespace");
+    this.key = Objects.requireNonNull(key, "key");
+    this.namespacedKey = namespace + ':' + key;
     this.id = id;
-    this.legacy = legacy;
-    this.flatting = flatting;
   }
 
-  Enchantment(int id, String identical) {
-    this(id, identical, identical);
+  @Contract("null, _ -> fail")
+  Enchantment(String key, @Nullable Integer id) {
+    this("minecraft", key, id);
   }
 
+  @Contract("null -> fail")
+  Enchantment(String key) {
+    this("minecraft", key, null);
+  }
+
+  @NotNull
+  public String getNamespace() {
+    return namespace;
+  }
+
+  @NotNull
+  public String getKey() {
+    return key;
+  }
+
+  @NotNull
+  public String getNamespacedKey() {
+    return namespacedKey;
+  }
+
+  @Deprecated
   public int getId() {
-    return id;
+    return id != null ? id : -1;
   }
 
-  @NotNull
-  public String getLegacy() {
-    return legacy;
+  @Override
+  public String toString() {
+    return namespacedKey;
   }
 
-  @NotNull
-  public String getFlatting() {
-    return flatting;
-  }
+  public final static Enchantment PROTECTION = new Enchantment("protection", 0);
+  public final static Enchantment FIRE_PROTECTION = new Enchantment("fire_protection", 1);
+  public final static Enchantment FEATHER_FALLING = new Enchantment("feather_falling", 2);
+  public final static Enchantment BLAST_PROTECTION = new Enchantment("blast_protection", 3);
+  public final static Enchantment PROJECTILE_PROTECTION = new Enchantment("projectile_protection", 4);
+  public final static Enchantment RESPIRATION = new Enchantment("respiration", 5);
+  public final static Enchantment AQUA_AFFINITY = new Enchantment("aqua_affinity", 6);
+  public final static Enchantment THORNS = new Enchantment("thorns", 7);
+  public final static Enchantment DEPTH_STRIDER = new Enchantment("depth_strider", 8);
+  public final static Enchantment FROST_WALKER = new Enchantment("frost_walker", 9);
+  public final static Enchantment BINDING_CURSE = new Enchantment("binding_curse", 10);
 
-  @SuppressWarnings("deprecation")
-  @NotNull
-  public org.bukkit.enchantments.Enchantment toBukkit() throws UnsupportedOperationException {
-    org.bukkit.enchantments.Enchantment enchantment;
-    if (BukkitVersion.isV113OrLater) {
-      enchantment = org.bukkit.enchantments.Enchantment.getByKey(org.bukkit.NamespacedKey.minecraft(flatting));
-    } else {
-      enchantment = org.bukkit.enchantments.Enchantment.getByName(legacy);
-    }
-    if (enchantment == null) throw new UnsupportedOperationException(
-      "Server version does not unsupported this enchantment: " + name());
-    return enchantment;
-  }
+  public final static Enchantment SHARPNESS = new Enchantment("sharpness", 16);
+  public final static Enchantment SMITE = new Enchantment("smite", 17);
+  public final static Enchantment BANE_OF_ARTHROPODS = new Enchantment("bane_of_arthropods", 18);
+  public final static Enchantment KNOCKBACK = new Enchantment("knockback", 19);
+  public final static Enchantment FIRE_ASPECT = new Enchantment("fire_aspect", 20);
+  public final static Enchantment LOOTING = new Enchantment("looting", 21);
+  public final static Enchantment SWEEPING = new Enchantment("sweeping", 22);
 
-  private final static Map<Integer, Enchantment> ID_MAP;
-  private final static Map<String, Enchantment> NAME_MAP;
+  public final static Enchantment EFFICIENCY = new Enchantment("efficiency", 32);
+  public final static Enchantment SILK_TOUCH = new Enchantment("silk_touch", 33);
+  public final static Enchantment UNBREAKING = new Enchantment("unbreaking", 34);
+  public final static Enchantment FORTUNE = new Enchantment("fortune", 35);
+  public final static Enchantment MENDING = new Enchantment("mending", 36);
+  public final static Enchantment VANISHING_CURSE = new Enchantment("vanishing_curse", 37);
 
-  static {
-    Map<Integer, Enchantment> idMap = new HashMap<>();
-    Map<String, Enchantment> nameMap = new HashMap<>();
-    for (Enchantment enchantment : Enchantment.values()) {
-      idMap.put(enchantment.id, enchantment);
-      nameMap.put(enchantment.legacy, enchantment);
-      nameMap.put(enchantment.flatting, enchantment);
-    }
-    ID_MAP = Collections.unmodifiableMap(idMap);
-    NAME_MAP = Collections.unmodifiableMap(nameMap);
-  }
+  public final static Enchantment POWER = new Enchantment("power", 48);
+  public final static Enchantment PUNCH = new Enchantment("punch", 49);
+  public final static Enchantment FLAME = new Enchantment("flame", 50);
+  public final static Enchantment INFINITE = new Enchantment("infinite", 51);
+  public final static Enchantment LUCK_OF_THE_SEA = new Enchantment("luck_of_the_sea", 61);
+  public final static Enchantment LURE = new Enchantment("lure", 62);
 
-  @SuppressWarnings("deprecation")
-  @NotNull
-  @Contract("null -> fail; !null -> !null")
-  public static Enchantment fromBukkit(org.bukkit.enchantments.Enchantment enchantment) {
-    Objects.requireNonNull(enchantment, "enchantment");
-    if (BukkitVersion.isV113OrLater) {
-      return NAME_MAP.get(enchantment.getKey().getKey());
-    } else {
-      return NAME_MAP.get(enchantment.getName());
-    }
-  }
+  public final static Enchantment LOYALTY = new Enchantment("loyalty");
+  public final static Enchantment IMPALING = new Enchantment("impaling");
+  public final static Enchantment RIPTIDE = new Enchantment("riptide");
+  public final static Enchantment CHANNELING = new Enchantment("channeling");
 
-  @Nullable
-  public static Enchantment fromId(int id) {
-    return ID_MAP.get(id);
-  }
-
-  @Nullable
-  @Contract("null -> null")
-  public static Enchantment fromName(@Nullable String name) {
-    if (name == null) return null;
-    return NAME_MAP.get(name.toLowerCase(Locale.ENGLISH));
-  }
+  public final static Enchantment MULTISHOT = new Enchantment("multishot");
+  public final static Enchantment QUICK_CHARGE = new Enchantment("quick_charge");
+  public final static Enchantment PIERCING = new Enchantment("piercing");
+  public final static Enchantment SOUL_SPEED = new Enchantment("soul_speed");
 }
